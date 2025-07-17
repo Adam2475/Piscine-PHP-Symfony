6dff5d0a4afa9d20b7de6758f0b66b81b3c2bb75
Adam2475 <adamnatanpassaretti@gmail.com>
2025-07-17 19:37:06 +0200

    done day05 ex00

--- day05/ex00/d05/src/ex00/Bundle/Controller/DefaultController.php
+++ day05/ex00/d05/src/ex00/Bundle/Controller/DefaultController.php
@@ -12,6 +12,36 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('ex00Bundle:Default:index.html.twig');
+        $message = null;
+        $host = 'localhost';
+        $user = 'symfony_user';
+        $password = 'symfony123';
+        $database = 'symfony_ex00';
+
+        $mysqli = new \mysqli($host, $user, $password, $database);
+
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(255) NOT NULL UNIQUE,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            enable BOOLEAN NOT NULL,
+            birthdate DATETIME NOT NULL,
+            address LONGTEXT NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+
+        if (isset($_GET['create']))
+        {
+            try {
+                $mysqli->query($sql);
+                $message = "Table 'users' created or already exists.";
+            } catch (\Exception $e) {
+                $message = "Error creating table: " . $e->getMessage();
+            }
+        }
+        
+        return $this->render('ex00Bundle:Default:index.html.twig', [
+            'message' => $message
+        ]);
     }
 }
