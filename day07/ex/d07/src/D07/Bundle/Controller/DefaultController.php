5243cc62433cf7f520c6d1c426bcb8aca4096d9d
Adam2475 <adamnatanpassaretti@gmail.com>
2025-07-25 13:38:42 +0200

    finished day07 ex00

---
+++ day07/ex/d07/src/D07/Bundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace D07\Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('D07Bundle:Default:index.html.twig');
+    }
+}
